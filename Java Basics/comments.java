35a1aafe3c34d689230e9f573cc15724fdbdf7c2
Vamshi Goud <vgoud55@gmail.com>
2019-11-12 23:18:41 +0530

    Added comment.java

---
+++ Java Basics/comments.java
@@ -0,0 +1,16 @@
+class comments
+{
+	public static void main(String args[])
+	{
+		//The x variable stores integer type of variable
+		int x= 1;
+
+		/*
+		This program is to demonstrate the use of comments in a program
+		There are two types of comments 
+		single line comment and multi-line comment
+		*/
+
+		System.out.println("Comments");
+	}
+}
