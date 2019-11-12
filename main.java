ddbe0d7e14fa6fe8d18e291b0c169d0b5bdd3a7d
Vamshi Goud <vgoud55@gmail.com>
2019-11-12 16:56:40 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,17 @@
+class main 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello world");
+	}
+
+	public static DisplayName()
+	{
+		System.out.println("No name is provided in the arguments");
+	}
+
+	public static DisplayName(String name)
+	{
+		System.out.println("The name is "+name);
+	}
+}
